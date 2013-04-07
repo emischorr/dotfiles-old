de55ae53dfdbe38e2851e3feb21b98196666e344
Enrico Mischorr <enrico@mischorr.de>
2013-04-07 15:09:02 +0200

    add node to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:$HOME/bin"
+PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:$HOME/bin"
+PATH=$PATH:/opt/node/bin
+export PATH
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
